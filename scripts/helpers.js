a6c43181f3036be73fdafa65ef63fbe10bd68d6a
xiaokai <kexiaokai@gmail.com>
2015-05-17 13:16:00 +0800

    [UPD] remove languages

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -12,32 +12,28 @@ function startsWith(str, start){
   return str.substring(0, start.length) === start;
 }
 
-
 hexo.extend.helper.register('page_nav', function(){
   var type = this.page.canonical_path.split('/')[0];
   var sidebar = this.site.data.sidebar[type];
   var path = pathFn.basename(this.path);
   var list = {};
-  var prefix = 'sidebar.' + type + '.';
 
   for (var i in sidebar){
     for (var j in sidebar[i]){
       list[sidebar[i][j]] = j;
     }
   }
-
   var keys = Object.keys(list);
   var index = keys.indexOf(path);
   var result = '';
-
   if (index > 0){
-    result += '<a href="' + keys[index - 1] + '" class="article-footer-prev" title="' + this.__(prefix + list[keys[index - 1]]) + '">' +
-      '<i class="fa fa-chevron-left"></i><span>' + this.__('page.prev') + '</span></a>';
+    result += '<a href="' + keys[index - 1] + '" class="article-footer-prev" title="' + list[keys[index - 1]] + '">' +
+      '<i class="fa fa-chevron-left"></i><span>prev</span></a>';
   }
 
   if (index < keys.length - 1){
-    result += '<a href="' + keys[index + 1] + '" class="article-footer-next" title="' + this.__(prefix + list[keys[index + 1]]) + '">' +
-      '<span>' + this.__('page.next') + '</span><i class="fa fa-chevron-right"></i></a>';
+    result += '<a href="' + keys[index + 1] + '" class="article-footer-next" title="' + list[keys[index + 1]] + '">' +
+      '<span>next</span><i class="fa fa-chevron-right"></i></a>';
   }
 
   return result;
@@ -45,22 +41,19 @@ hexo.extend.helper.register('page_nav', function(){
 
 hexo.extend.helper.register('doc_sidebar', function(className){
   var type = this.page.canonical_path.split('/')[0];
-  var type = 'zh-tw'
   var sidebar = this.site.data.sidebar[type];
-  console.log(sidebar)
   var path = pathFn.basename(this.path);
   var result = '';
   var self = this;
-  var prefix = 'sidebar.' + type + '.';
 
   _.each(sidebar, function(menu, title){
-    result += '<strong class="' + className + '-title">' + self.__(prefix + title) + '</strong>';
+    result += '<strong class="' + className + '-title">' + title + '</strong>';
 
     _.each(menu, function(link, text){
       var itemClass = className + '-link';
       if (link === path) itemClass += ' current';
 
-      result += '<a href="' + link + '" class="' + itemClass + '">' + self.__(prefix + text) + '</a>';
+      result += '<a href="' + link + '" class="' + itemClass + '">' + text + '</a>';
     })
   });
 
@@ -71,25 +64,14 @@ hexo.extend.helper.register('header_menu', function(className){
   var menu = this.site.data.menu;
   var result = '';
   var self = this;
-  var lang = this.page.lang;
-  var isEnglish = lang === 'en';
 
-  _.each(menu, function(path, title){
-    if (!isEnglish && ~localizedPath.indexOf(title)) path = lang + path;
-
+  _.each(menu, function (path, title){
     result += '<li class="' + className + '-item">';
-    result += '<a href="' + self.url_for(path) + '" class="' + className + '-link">' + self.__('menu.' + title) + '</a>';
+    result += '<a href="' + self.url_for(path) + '" class="' + className + '-link">' + title + '</a>';
     result += '</li>';
   });
 
   return result;
-});
-
-hexo.extend.helper.register('canonical_url', function(lang){
-  var path = this.page.canonical_path;
-  if (lang && lang !== 'en') path = lang + '/' + path;
-
-  return this.config.url + '/' + path;
 });
 
 hexo.extend.helper.register('raw_link', function(path){
@@ -126,14 +108,4 @@ hexo.extend.helper.register('lunr_index', function(data){
   });
 
   return JSON.stringify(index.toJSON());
-});
-
-hexo.extend.helper.register('canonical_path_for_nav', function(){
-  var path = this.page.canonical_path;
-
-  if (startsWith(path, 'docs/') || startsWith(path, 'api/')){
-    return path;
-  } else {
-    return '';
-  }
 });
