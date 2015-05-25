adfc627c01e662c0fb7710e8acc23396efc8af54
xiaokai <kexiaokai@gmail.com>
2015-05-25 20:43:41 +0800

    [UPD] delete templete unuser function

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -6,7 +6,6 @@ var url = require('url');
 var cheerio = require('cheerio');
 var lunr = require('lunr');
 
-var localizedPath = ['docs', 'api'];
 
 function startsWith(str, start){
   return str.substring(0, start.length) === start;
